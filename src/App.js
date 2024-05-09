4b20bff8bbacf3621bd4a9e924b84119a6fc1b65
Austin Perry <austinjperry22@gmail.com>
2024-05-08 19:56:41 -0600

    basic layout and structure done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Layout from "./Components/Layout";
 import Public from "./Components/Public";
 import Login from './features/auth/Login'
 import DashLayout from "./Components/DashLayout";
+import Welcome from "./features/auth/Welcome";
+import NotesList from "./features/notes/NotesList";
+import UsersList from "./features/users/UsersList";
+
 
 function App() {
   return (
@@ -12,10 +16,18 @@ function App() {
         <Route index element={<Public />} />
         <Route index element={<Login />} />
 
-
         <Route path="dash" element={<DashLayout />}>
           
-        </Route>
+          <Route index element={<Welcome />}/>
+          <Route path="notes">
+            <Route index element={<NotesList />}/>
+          </Route>
+
+          <Route path="users">
+            <Route index element={<UsersList />}/>
+          </Route>
+
+        </Route>{/*End Dash*/}
       </Route>
     </Routes> 
   );
--- src/Components/DashHeader.js
+++ src/Components/DashHeader.js
@@ -7,8 +7,8 @@ const DashHeader = () => {
   const content = (
     <header className='dash-header'>
         <div className='dash-header__container'>
-            <Link to="/dash/notes">
-                <h1 className='dash-header__titile'>techNotes</h1>
+            <Link to="/dash">
+                <h1 className='dash-header__title'>techNotes</h1>
             </Link>
             <nav className='dash-header__nav'>
                 {/* {add nav buttons later} */}
